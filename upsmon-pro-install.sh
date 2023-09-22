ae41dbc9ba8246e0103bac1bde4436a0d5c22567
nifihu <145766770+nifihu@users.noreply.github.com>
2023-09-22 16:59:22 +0400

    Update upsmon-pro-install.sh

--- upsmon-pro-install.sh
+++ upsmon-pro-install.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 wget https://www.upspowercom.com/PRO-Linux/UPSMON_PRO_Linux_V1.40.tar
-sudo apt-get install libc6-i386
-sudo apt install default-jre
+sudo apt-get install libc6-i386 -y
+sudo apt install default-jre -y
 tar -xvf UPSMON_PRO_Linux_V1.40.tar
 tar -xvf UPSMON_PRO_Linux.tar
 cd UPSMON_PRO_Linux
